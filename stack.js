6a25e8d1a7ad29dbf24a5869ee93138763558d09
Gagan Singh <gagansingh.dev@outlook.com>
2020-02-11 11:17:47 -0500

    added drills

--- stack.js
+++ stack.js
@@ -11,27 +11,63 @@ class Stack {
     }
 
     push(data) {
-        /* If the stack is empty, then the node will be the
-           top of the stack */
         if (this.top === null) {
             this.top = new _Node(data, null);
             return this.top;
         }
 
-        /* If the stack already has something, 
-           then create a new node,
-           add data to the new node, and
-           have the pointer point to the top */
         const node = new _Node(data, this.top);
         this.top = node;
     }
 
     pop() {
-        /* In order to remove the top of the stack, you have to point
-           the pointer to the next item and that next item becomes the
-           top of the stack */
         const node = this.top;
         this.top = node.next;
         return node.data;
     }
 }
+
+const starTrek = new Stack();
+starTrek.push("Kirk");
+starTrek.push("Spock");
+starTrek.push("McCoy");
+starTrek.push("Scotty");
+
+function peek(stack) {
+    if (!stack.top) {
+        return;
+    }
+    return stack.top;
+}
+
+function isEmpty(stack) {
+    if (!stack.top) {
+        console.log("Stack is empty");
+        return;
+    }
+    return "Stack is not empty";
+}
+
+function display(stack) {
+    if (!stack.top) {
+        console.log("Stack is empty");
+        return;
+    }
+    let current = stack.top;
+    while (current !== null) {
+        console.log(current.data);
+        current = current.next;
+    }
+    return;
+}
+
+function is_palindrome(s) {
+    s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    // Your code goes here
+}
+
+// True, true, true, false
+console.log(is_palindrome("dad"));
+console.log(is_palindrome("A man, a plan, a canal: Panama"));
+console.log(is_palindrome("1001"));
+console.log(is_palindrome("Tauhida"));
